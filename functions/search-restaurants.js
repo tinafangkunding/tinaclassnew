b940734a2102b0345dcb3d4fae23bbb367b2c64d
tinafangkunding <tinafang@tencent.com>
2019-05-14 00:33:16 +0800

    firstcom

---
+++ functions/search-restaurants.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const co = require("co");
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const defaultResults = process.env.defaultResults || 8;
+const tableName = process.env.restaurants_table;
+
+function* findRestaurantByTheme(theme, count) {
+    let req = {
+        TableName: tableName,
+        Limit: count,
+        FilterExpression: "contains(themes, :theme)",
+        ExpressionAttributeValues: {":theme": theme}
+    };
+
+    let resp = yield dynamodb.scan(req).promise();
+    return resp.Items;
+}
+
+module.exports.handler = co.wrap(function* (event, context, cb) {
+    let req = JSON.parse(event.body);
+    let restaurants = yield findRestaurantByTheme(req.theme, defaultResults);
+    let response = {
+        statusCode: 200,
+        body: JSON.stringify(restaurants)
+    }
+    cb(null, response);
+});
---
+++ tests/steps/init.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const co = require('co');
+const Promise = require('bluebird');
+const awscred = Promise.promisifyAll(require('awscred'));
+
+let initialized = false;
+
+let init = co.wrap(function*(){
+    if(initialized){
+        return;
+    }
+
+    process.env.restaurants_api = "https://vo6fha5uj4.execute-api.us-east-1.amazonaws.com/dev/restaurants";
+    process.env.restaurants_table = "restaurants2";
+    process.env.AWS_REGION = "us-east-1";
+    process.env.cognito_client_id = "test_cognito_client_id";
+    process.env.cognito_user_pool_id = "us-east-1_plji5Qpmj";
+    process.env.cognito_server_client_id = "3dpcquk86lnjgcrpsh6vfr5o5b";
+
+    let cred = (yield awscred.loadAsync()).credentials;
+    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+
+    console.log("AWS credentials loaded");
+    
+    if(cred.sessionToken){
+        process.env.AWS_SESSION_TOKEN = cred.sessionToken;
+      }  
+
+    initialized = true;
+});
+
+module.exports.init = init;
